8eba944c28a6ee34587c2bd00b0460bc14af78ed
sailzeng <fullsail@163.com>
2016-06-30 02:17:35 +0800

    增加代码。实验proto的扩展属性功能。

--- src/raper/raper_main.cpp
+++ src/raper/raper_main.cpp
@@ -33,6 +33,28 @@ int main(int argc, char *argv[])
 	QString error_tips;
 	QString allinone_path = "E:/Courage/illusion.git/example/001";
 
+	const struct option RAPER_LONG_OPTION[]=
+	{
+		{ "config", required_argument,NULL, 'c' },
+		{ "excel", required_argument, NULL,'x' },
+		{ "proto", required_argument,NULL, 'p' },
+		{ "outer", required_argument,NULL, 'o' },
+		{ "help", no_argument,NULL, 'h' },
+		{ NULL, no_argument, NULL, 0 }
+	};
+	int long_index = 0;
+	const char RAPER_OPT_STRING[] = "c:x:p:o:h";
+	int opt = -1;
+	while (-1 != (opt = ZCE_LIB::getopt_long(argc, argv,
+											 RAPER_OPT_STRING,
+											 RAPER_LONG_OPTION,
+											 &long_index)))
+	{
+		switch (opt) 
+		{
+
+		};
+	}
 	ret = Biko_Read_Config::instance()->init_read_all2(allinone_path,
 													   error_tips);
 	if (ret != 0)
